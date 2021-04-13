e58e74b01d7c91c8bee9e81b15a1e2b78b88c563
anish <aanishmv@gmail.com>
2021-04-14 01:02:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState , useEffect} from 'react'
+import "./App.css"
+import Recipe from './components/recipe'
 
-function App() {
+
+const App = () => {
+
+  const App_id = "ee111e4c"
+  const App_key ="6599db68907bd051c40cf2b533fb4932"
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState("")
+  const [query, setQuery] = useState("")
+  const [load,setLoad] = useState(undefined)
+  
+  useEffect(() => {
+
+    getRecipes()
+    setLoad(false)
+    
+  }, [query])
+
+  
+  const getRecipes = async() => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${App_id}&app_key=${App_key}&from=0&to=3&calories=591-722&health=alcohol-free`)
+    const data = await response.json()
+    console.log(data)
+    setLoad(true)
+    
+    
+    setRecipes(data.hits)
+  }
+  const updateInput = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    
+    setQuery(search)
+    e.preventDefault()
+    
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">KANIKA's KITCHEN!!!</h1>
+      <h4>Cook it bitch!!!</h4>
+      <form onSubmit ={getSearch } className ="search-form">
+        <input className ="search-input"type="text" onChange ={updateInput} />
+        <button  className ="search-button"type ="submit">search</button>
+      </form>
+      <div className ="getting-recipe">
+        {recipes.map(recipe => (
+          <Recipe ingredients = {recipe.recipe.ingredients} load={load} setLoad={setLoad}image={recipe.recipe.image} title={recipe.recipe.label} calorie={ recipe.recipe.calories}/>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/recipe.js
@@ -0,0 +1,36 @@
+import React,{useEffect, useState} from 'react'
+import style from './recipe.module.css'
+import Loader from 'react-loader-spinner'
+
+const Recipe = ({ title, image, calorie,load ,ingredients}) => {
+    const getIngredients = () => {
+        console.log("ingredieints are here my firend")
+        
+    }
+    return (
+        
+        <div className={style.recipe}>
+            <div className={style.container}>
+                {!load ?
+                    <Loader color="lightgrey" type="Bars" rotate={1 }className ={style.loader}>
+                        <img id={style.img} src={image} alt="" /> 
+                        <h1 className={ style.foodname}>  {title}</h1>
+                        <p className={style.calorie}>Calorie:{calorie}</p>
+                    </Loader> :
+                    <div>
+                        <img onClick={getIngredients} id={style.img} src={image} alt="loading" />
+                        <h1 className={ style.foodname}>  {title}</h1>
+                        <p className={style.calorie}>Calorie:{calorie}</p>
+                        <ul id ={style.ingredients}>
+                            {ingredients.map((ingredient,index) => (
+                                <li key={index}>{ingredient.text}</li>
+                            ))}
+                        </ul>
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Recipe;
